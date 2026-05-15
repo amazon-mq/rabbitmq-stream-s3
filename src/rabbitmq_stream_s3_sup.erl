9f29f433195a8ea28e04ab3a8e56668dfa52cc4c
Michael Davis <mcrdavis@amazon.com>
2026-05-15 12:36:54 -0400

    Move the HTTP pools to the overall supervisor

--- src/rabbitmq_stream_s3_sup.erl
+++ src/rabbitmq_stream_s3_sup.erl
@@ -4,52 +4,34 @@
 -module(rabbitmq_stream_s3_sup).
 -behaviour(supervisor).
 
--export([start_link/0, start_pools/0]).
+-export([start_link/0]).
 -export([init/1]).
-
--rabbit_boot_step(
-    {rabbitmq_stream_s3_http_pools, [
-        {description, "S3 HTTP connection pools"},
-        {mfa, {?MODULE, start_pools, []}},
-        {requires, core_initialized}
-    ]}
-).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-start_pools() ->
-    UploadPoolId = rabbitmq_stream_s3_upload_pool,
-    ok = rabbit_sup:start_child(UploadPoolId, rabbitmq_stream_s3_api_aws_pool, [
-        UploadPoolId,
-        #{
-            name => UploadPoolId,
-            min_size => rabbitmq_stream_s3_config:upload_pool_min_size(),
-            max_size => rabbitmq_stream_s3_config:upload_pool_max_size()
-        }
-    ]),
-    GeneralPoolId = rabbitmq_stream_s3_general_pool,
-    ok = rabbit_sup:start_child(GeneralPoolId, rabbitmq_stream_s3_api_aws_pool, [
-        GeneralPoolId,
-        #{
-            name => GeneralPoolId,
-            min_size => rabbitmq_stream_s3_config:general_pool_min_size(),
-            max_size => rabbitmq_stream_s3_config:general_pool_max_size()
-        }
-    ]).
-
 init([]) ->
     rabbitmq_stream_s3_registry:init(),
     SupFlags = #{strategy => one_for_one, intensity => 3, period => 5},
-    ManifestCache = #{
-        id => rabbitmq_stream_s3_manifest_replica,
-        type => worker,
-        start => {rabbitmq_stream_s3_manifest_replica, start_link, []}
-    },
+    UploadPool = pool_child(rabbitmq_stream_s3_upload_pool, #{
+        name => rabbitmq_stream_s3_upload_pool,
+        min_size => rabbitmq_stream_s3_config:upload_pool_min_size(),
+        max_size => rabbitmq_stream_s3_config:upload_pool_max_size()
+    }),
+    GeneralPool = pool_child(rabbitmq_stream_s3_general_pool, #{
+        name => rabbitmq_stream_s3_general_pool,
+        min_size => rabbitmq_stream_s3_config:general_pool_min_size(),
+        max_size => rabbitmq_stream_s3_config:general_pool_max_size()
+    }),
     Reaper = #{
         id => rabbitmq_stream_s3_reaper,
         type => worker,
         start => {rabbitmq_stream_s3_reaper, start_link, []}
+    },
+    ManifestCache = #{
+        id => rabbitmq_stream_s3_manifest_replica,
+        type => worker,
+        start => {rabbitmq_stream_s3_manifest_replica, start_link, []}
     },
     ReplicaReaderSup = #{
         id => rabbitmq_stream_s3_replica_reader_sup,
@@ -61,5 +43,19 @@ init([]) ->
         type => worker,
         start => {rabbitmq_stream_s3_membership_reconciliation, start_link, []}
     },
-    Procs = [ManifestCache, Reaper, ReplicaReaderSup, MembershipReconciliation],
+    Procs = [
+        ManifestCache,
+        Reaper,
+        MembershipReconciliation,
+        UploadPool,
+        GeneralPool,
+        ReplicaReaderSup
+    ],
     {ok, {SupFlags, Procs}}.
+
+pool_child(Id, Config) ->
+    #{
+        id => Id,
+        type => worker,
+        start => {rabbitmq_stream_s3_api_aws_pool, start_link, [Id, Config]}
+    }.
